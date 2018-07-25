7cad17763d27022e713fbc4d1dabef083ac65c3e
liranfar <l.farage@gmail.com>
2018-07-25 18:48:44 +0300

    integrated mongoose

--- services/server/server.js
+++ services/server/server.js
@@ -1,7 +1,29 @@
-var app = require('./src/app');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const customers = require('./routes/customer')
 
-const port = 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+// Connect to db
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+app.use('/api/v1/customers', customers);
+
+
+app.listen(port, () => console.log(`Server running on port ${port}, mongodb uri: ${process.env.MONGO_DB_URI}`));
 
 //TODO: add winston logger https://github.com/winstonjs/winston
+//TODO: use mongoose
